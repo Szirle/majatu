5426ad38fc61c92f6857baf7537cf4a5b8e21f0f
Szymon Łukiewicz <33434971+Szirle@users.noreply.github.com>
2023-09-15 19:49:13 +0200

    dodano sprawdzanie odpowiedzi

--- app/src/main/java/com/example/majatu/activities/Level2Activity.kt
+++ app/src/main/java/com/example/majatu/activities/Level2Activity.kt
@@ -1,52 +1,69 @@
 package com.example.majatu.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.majatu.R
 import com.example.majatu.classes.FiszkaLevel2
 import kotlin.random.Random
+import kotlin.time.measureTime
 
 class Level2Activity : AppCompatActivity() {
+    val listaFiszek = listOf<FiszkaLevel2>(
+        FiszkaLevel2(
+            polskieSlowo = "Auto",
+            gitTlumaczenie = "La Macchina",
+            listOf("Wrooooom", "Machinacina", "L'aereoaf")
+        ),
+        FiszkaLevel2(
+            polskieSlowo = "Dziena",
+            gitTlumaczenie = "Grazie",
+            listOf("Dziaaa", "Per faworek", "Ciao")
+        ),
+        FiszkaLevel2(
+            polskieSlowo = "Przepraszam",
+            gitTlumaczenie = "Spiacente",
+            listOf("Grazie mille", "Oi!", "Scuse moi :(")
+        ),
+        FiszkaLevel2(
+            polskieSlowo = "Niunia",
+            gitTlumaczenie = "La ragazza",
+            listOf("El Nino", "La Hanamontana", "Il Uomo")
+        )
+    )
+    val przyciskOdp1: Button = findViewById(R.id.button)
+    val przyciskOdp2: Button = findViewById(R.id.button2)
+    val przyciskOdp3: Button = findViewById(R.id.button3)
+    val przyciskOdp4: Button = findViewById(R.id.button4)
+    val polskieSlowo: TextView = findViewById(R.id.polskieSlowoTextView)
+
+    var numerPokazywanejFiszki: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level_2)
 
         wyswietlFiszki()
+
     }
 
 
+    fun wyswietlFiszki() {
 
-    fun wyswietlFiszki(){
 
-        val listaFiszek = listOf<FiszkaLevel2>(
-            FiszkaLevel2(polskieSlowo = "Auto", gitTlumaczenie = "La Macchina", listOf("Wrooooom", "Machinacina", "L'aereoaf")),
-            FiszkaLevel2(
-                polskieSlowo = "Dziena",
-                gitTlumaczenie = "Grazie",
-                listOf("Dziaaa", "Per faworek", "Ciao")
-            ),
-            FiszkaLevel2(
-                polskieSlowo = "Przepraszam",
-                gitTlumaczenie = "Spiacente",
-                listOf("Grazie mille", "Oi!", "Scuse moi :(")
-            ),
-            FiszkaLevel2(
-                polskieSlowo = "Niunia",
-                gitTlumaczenie = "La ragazza",
-                listOf("El Nino", "La Hanamontana", "Il Uomo")
-            )
-        )
-        val przyciskOdp1 : Button = findViewById(R.id.button)
-        val przyciskOdp2 : Button = findViewById(R.id.button2)
-        val przyciskOdp3 : Button = findViewById(R.id.button3)
-        val przyciskOdp4 : Button = findViewById(R.id.button4)
-        val polskieSlowo : TextView = findViewById(R.id.polskieSlowoTextView)
+        val batonList: List<Button> = listOf(przyciskOdp1, przyciskOdp2, przyciskOdp3, przyciskOdp4)
+        val batonDalej: Button = findViewById(R.id.dawajDalej)
+        batonDalej.visibility = View.INVISIBLE
 
-        val wyswietlanaFiszka = listaFiszek[0]
+        var wyswietlanaFiszka: FiszkaLevel2 = listaFiszek[numerPokazywanejFiszki]
         polskieSlowo.text = wyswietlanaFiszka.polskieSlowo
         val numerBatona: Int = Random.nextInt(1, 5)
+        val nayOrSlay: TextView = findViewById(R.id.sprawdzam)
+        nayOrSlay.visibility = View.INVISIBLE
+
+
 
         if (numerBatona == 1) {
             przyciskOdp1.text = wyswietlanaFiszka.gitTlumaczenie
@@ -71,6 +88,27 @@ class Level2Activity : AppCompatActivity() {
             przyciskOdp1.text = wyswietlanaFiszka.zleTlumaczenia[0]
             przyciskOdp2.text = wyswietlanaFiszka.zleTlumaczenia[1]
             przyciskOdp3.text = wyswietlanaFiszka.zleTlumaczenia[2]
+        }
+
+        batonList.forEach {
+            val text = it.text
+            it.setOnClickListener {
+                if (text == wyswietlanaFiszka.gitTlumaczenie) {
+                    nayOrSlay.visibility = View.VISIBLE
+                    nayOrSlay.text = "SLAY!"
+                    batonDalej.visibility = View.VISIBLE
+                    batonDalej.setOnClickListener {
+                        numerPokazywanejFiszki += 1
+                        wyswietlFiszki()
+                    }
+                } else {
+                    nayOrSlay.visibility = View.VISIBLE
+                    nayOrSlay.text = "HELL NAH"
+                    batonDalej.visibility = View.INVISIBLE
+                }
+            }
+
+
         }
 
 
